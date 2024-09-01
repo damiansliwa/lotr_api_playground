9b99028883e38386d3406dd3dc3d9bcc4128f0e1
Damian Śliwa <damian.sliwa.90@gmail.com>
2024-09-01 10:46:12 +0200

    Manage character race name fetching.

--- app/controllers/api/v1/external_api_controller.rb
+++ app/controllers/api/v1/external_api_controller.rb
@@ -8,21 +8,36 @@ module Api
 				response = HTTParty.get("https://lotrapi.co/api/v1/characters/#{character_id}")
 
 				if response.success?
-					character_name = JSON.parse(response.body)["name"]
-					character = Character.find_by(name: character_name)
+					ext_character_data = JSON.parse(response.body)
+					ext_character_name = ext_character_data["name"]
+					ext_character_race = ext_character_data["race"]
+					ext_race_url = HTTParty.get(ext_character_race)
+					ext_race_name = JSON.parse(ext_race_url.body)["name"]
+					ext_character_realm = ext_character_data["realm"]
+					ext_realm_url = HTTParty.get(ext_character_realm)
+					ext_realm_name = JSON.parse(ext_realm_url.body)["name"]
+			
+					character = Character.find_by(name: ext_character_name)
 					if character
 						render json: { 
-							message: "Yaaay! Character was found on local database!",
-							name: character_name,
+							message: "This character already exists on local database! See details below:",
+							name: character.name,
 							race: character.race,
 							realm: character.realm
-						}
+						},
 						status: :ok
 					else
+						imported_character = Character.create(
+							name: ext_character_data["name"],
+							race: ext_race_name,
+							realm: ext_realm_name
+							)
 						render json: { 
-							message: "Nay! :( This character doesn't exist on local database.",
-							name: character_name
-						}
+							message: "New character has been imported to the local database!",
+							name: imported_character.name,
+							race: imported_character.race,
+							realm: imported_character.realm
+						},
 						status: :ok
 					end
 				else
